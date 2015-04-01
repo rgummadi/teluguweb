e781d30d9dc618c1ff28112b24e09f5d4d3aa6e5
Ramesh Gummadi <rgummadi@Admins-MacBook-Pro.local>
2015-04-01 10:30:33 +0530

    6

--- teluguweb/settings.py
+++ teluguweb/settings.py
@@ -74,8 +74,8 @@ _MONGO_DB_NAME = 'teluguweb'
 _MONGO_DB_HOST = \
     'mongodb://%s' \
     %(_MONGO_HOST,)
-
-mongoengine.connect(_MONGO_DB_NAME)
+_MONGO_URI = "mongodb://heroku_app35242924:90s0k6rnu5a9iulpc1lqbf3327@ds059471.mongolab.com:59471/heroku_app35242924"
+mongoengine.connect(_MONGO_URI)
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
